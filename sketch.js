d36efc9c5fef74751eeaec04cd7b355f9f1b5205
Laisfernanda-04 <00001124541925sp@al.educacao.sp.gov.br>
2024-09-24 15:06:56 -0300

    arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,11 @@
+let angle = 0;
+let gridSize = 5; // Number of cubes in each row and column
+let cubeSize = 40; // Size of each cube
+
+function setup() {
+  createCanvas(400, 400, WEBGL);
+  noFill();
+}
+
+function draw() {
+  background(220);
